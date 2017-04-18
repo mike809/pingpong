10306790965f221772bdf9cd0bd5fcf7d42b5338
Michael Romano <michael.romano@lendup.com>
2017-04-17 23:21:48 -0700

    add user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,7 @@
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #  rating                 :integer          default("1000")
+#  first_name             :string
 #
 
 require 'rails_helper'
@@ -26,6 +27,20 @@ RSpec.describe User, type: :model do
   context 'when we create a new user' do
     it 'has a raking of 1000' do
       expect(user.rating).to eq 1000
+    end
+  end
+
+  context 'when we have multiple users' do
+    let(:other_user) { FactoryGirl.create(:user, rating: 1500) }
+    let(:sorted_users) { User.unscoped.sort_by(&:score).reverse }
+
+    before do
+      user.save
+      other_user
+    end
+
+    it 'orders them by score as default scope' do
+      expect(User.all).to eq sorted_users
     end
   end
 end
