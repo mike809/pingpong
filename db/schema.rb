70c8988e8b5bda111e2dd3d507ca202cde389ce1
Michael Romano <michael.romano@lendup.com>
2017-04-17 22:42:30 -0700

    add first name to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418025655) do
+ActiveRecord::Schema.define(version: 20170418050626) do
 
   create_table "games", force: :cascade do |t|
     t.integer  "player_1_id"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170418025655) do
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
     t.integer  "rating",                 default: 1000
+    t.string   "first_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user, aliases: [:player_1, :player_2] do
+    first_name { Faker::Name.first_name }
     email { Faker::Internet.email }
     password { Faker::Internet.password }
   end
